8251e8cece3e64bfafddc387a015b30e5dadf458
nataliaamartinez <nataliamartinezluna17@gmail.com>
2026-05-27 19:10:13 +0200

    creacion de la pagina de diseños publicados

--- app/disenos-publicados/page.tsx
+++ app/disenos-publicados/page.tsx
@@ -72,7 +72,9 @@ export default function SubirDisenosPage() {
 
   return (
     <div className="max-w-xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-6">
+      <h1 className="text-2xl font-bold mb-6" style="
+    color:white;"
+>
         Subir diseño publicado 🎨
       </h1>
 
