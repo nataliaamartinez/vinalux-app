7fb682d3c8a3f338e2bea4449f2b7546ca246168
nataliaamartinez <nataliamartinezluna17@gmail.com>
2026-05-27 20:09:06 +0200

    creacion de la pagina de diseños publicados

--- app/disenos-publicados/page.tsx
+++ app/disenos-publicados/page.tsx
@@ -192,7 +192,7 @@ const generarPDF = async () => {
         </button>
       </div>
 
-      <h1 className="text-2xl font-bold mb-6">
+      <h1 className="text-2xl font-bold mb-6 text-white">
         Subir diseño publicado 🎨
       </h1>
 
